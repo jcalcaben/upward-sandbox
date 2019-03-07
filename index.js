71394fed5bbe3a0e4f14ad807071efb20e8e1476
James Calcaben <calcaben@adobe.com>
2019-03-07 15:23:53 -0600

    Create a simple 'Hello World' upward-spec using mustache templates

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ const {createUpwardServer, middleware} = require('@magento/upward-js');
 
 // Launching UPWARD using the `createUpwardServer` function
 
-// createUpwardServer({
-//   upwardPath: './upward-spec.yml',
-//   bindLocal: true,
-//   logUrl: true,
-//   port: 4000,
-// });
+createUpwardServer({
+  upwardPath: './hello-world-upward-spec.yml',
+  bindLocal: true,
+  logUrl: true,
+  port: 4000,
+});
 
 // Launching UPWARD-js using the `middleware` object
 //
-const app = express();
-
-middleware('/Users/calcaben/tmp/simple-upward/upward-spec.yml').then(
-  handler => {
-    app.use(handler);
-    app.listen(4000);
-  },
-  error => {
-    console.log('Failed!', error);
-  },
-);
+//const app = express();
+//
+//middleware('/Users/calcaben/tmp/simple-upward/upward-spec.yml').then(
+//  handler => {
+//    app.use(handler);
+//    app.listen(4000);
+//  },
+//  error => {
+//    console.log('Failed!', error);
+//  },
+//);
