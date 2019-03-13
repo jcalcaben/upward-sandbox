e83b173d4f5648592881f74b6157e89dd9feb3c6
James Calcaben <jcalcaben@gmail.com>
2019-03-13 00:03:09 -0500

    Fix React syntax error

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom'
-import {App} from './components'
+import ReactDOM from 'react-dom';
+import {App} from './components';
 
-ReactDOM.render(App, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
